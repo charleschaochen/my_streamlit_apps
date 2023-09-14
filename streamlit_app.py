cb305466e31b0a60cac56d37c2082a0ab5804b20
陈超 <399931011@qq.com>
2023-09-14 15:30:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,16 @@
 import streamlit as st
 import openai
 
+
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+
+
 # 设置OpenAI API密钥
-openai.api_key = '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'
+openai.api_key = openai_api_key
 
 # 创建一个输入框，让用户输入他们的问题
 question = st.text_input("Enter your question")
