a6cf3cb94daed0f6acf457165612443f31bb0196
陈超 <399931011@qq.com>
2023-09-14 15:27:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import openai
 
 # 设置OpenAI API密钥
-openai.api_key = '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'
+openai.api_key = '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'
 
 # 创建一个输入框，让用户输入他们的问题
 question = st.text_input("Enter your question")
